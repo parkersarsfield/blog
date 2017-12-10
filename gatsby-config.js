0343a51c8f7ae4b54d265e1fc383eeefcdead227
Parker Sarsfield <parker.h.sarsfield@vanderbilt.edu>
2017-12-09 18:17:40 -0600

    Add typography plugin.

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    siteMetadata: {
+        title: "Parker Sarsfield",
+        author: "Parker Sarsfield"
+    },
+    plugins: [
+        'gatsby-transformer-remark',
+        {
+            resolve: 'gatsby-plugin-typography',
+            options: {
+                pathToConfigModule: 'src/utils/typography',
+            }
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'src',
+                path: `${__dirname}/src`
+            }
+        }
+    ]
+}
